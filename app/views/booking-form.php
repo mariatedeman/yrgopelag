8ecde1d5e9a0cd5514414bd8fa7740c066ad8b40
mariatedeman <m.tedeman@hotmail.com>
2025-12-16 10:48:48 +0100

    Calender updated with min and max to only show january

--- app/views/booking-form.php
+++ app/views/booking-form.php
@@ -73,12 +73,12 @@ if (isset($_POST['name'], $_POST['transfer_code'], $_POST['checkin'], $_POST['ch
     <input type="text" name="transfer_code" placeholder="Type your transfer code">
 
     <label for="checkin">Chose arrival date</label>
-    <input type="date" name="checkin" id="">
+    <input type="date" name="checkin" min="2026-01-01" max="2026-01-31">
 
     <label for="checkout">Chose departure date</label>
-    <input type="date" name="checkout" id="">
+    <input type="date" name="checkout" min="2026-01-01" max="2026-01-31">
 
-    <label for="room_type">Chose type of room</label>
+    <label for=" room_type">Chose type of room</label>
     <select name="room_type" id="">
         <option value="1">Budget</option>
         <option value="2">Standard</option>
