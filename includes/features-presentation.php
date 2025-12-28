9ddb9b8a1b16014c6dc472fe3a94a18a26209634
mariatedeman <m.tedeman@hotmail.com>
2025-12-28 13:45:47 +0100

    some files moved from app/views/ to /includes/. features grid implemented, next step: connect info from DB

---
+++ includes/features-presentation.php
@@ -0,0 +1,52 @@
+<section class="features-presentation">
+    <h2>Our features</h2>
+    <section class="features-grid-container">
+
+        <div>
+            <span class="feature-img-container">
+                <img src="/assets/images/room_budget_mood.jpg" alt="">
+
+            </span>
+            <span class="feature-presentation-text">
+                <h4>Feature name</h4>
+                <p>Feature description</p>
+            </span>
+        </div>
+        <div>
+            <span class="feature-img-container">
+                <img src="/assets/images/room_budget_mood.jpg" alt="">
+            </span>
+            <span class="feature-presentation-text">
+                <h4>Feature name</h4>
+                <p>Feature description</p>
+            </span>
+        </div>
+        <div>
+            <span class="feature-img-container">
+                <img src="/assets/images/room_budget_mood.jpg" alt="">
+            </span>
+            <span class="feature-presentation-text">
+                <h4>Feature name</h4>
+                <p>Feature description</p>
+            </span>
+        </div>
+        <div>
+            <span class="feature-img-container">
+                <img src="/assets/images/room_budget_mood.jpg" alt="">
+            </span>
+            <span class="feature-presentation-text">
+                <h4>Feature name</h4>
+                <p>Feature description</p>
+            </span>
+        </div>
+        <div>
+            <span class="feature-img-container">
+                <img src="/assets/images/room_budget_mood.jpg" alt="">
+            </span>
+            <span class="feature-presentation-text">
+                <h4>Feature name</h4>
+                <p>Feature description</p>
+            </span>
+        </div>
+    </section>
+</section>
